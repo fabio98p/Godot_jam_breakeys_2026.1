ead24c70e463d93250db5cb1ef016e86369a7b28
fabio parisi <parisifabio98@gmail.com>
2026-02-15 17:23:20 +0100

    add some prototypes and minor init projects

---
+++ Player/player.gd
@@ -0,0 +1,54 @@
+extends CharacterBody3D
+
+@export var speed = 5.0
+@export var jump_velocity = 4.5
+@export var sensivity = 0.005
+
+var top_visibility_head = 60
+var bottom_visibility_head = -80
+
+@onready var head: Node3D = $Head
+@onready var camera_3d: Camera3D = $Head/Camera3D
+@onready var ray_cast_3d: RayCast3D = $Head/RayCast3D
+
+func _ready() -> void:
+	ray_cast_3d
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		
+	# handle esc
+	if Input.is_action_just_pressed("esc"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("left", "right", "forward", "back")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+
+	move_and_slide()
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		get_window().grab_focus()
+		
+	elif event is InputEventMouseMotion:
+		rotation.y -= event.relative.x * sensivity
+		head.rotation.x -= event.relative.y * sensivity
+		head.rotation_degrees.x = clamp(head.rotation_degrees.x, bottom_visibility_head, top_visibility_head)
+
+	# Print the size of the viewport.
+	# print("Viewport Resolution is: ", get_viewport().get_visible_rect().size)
