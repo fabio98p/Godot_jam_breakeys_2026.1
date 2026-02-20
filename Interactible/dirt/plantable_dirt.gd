d861a5965bcac13e76f0a04509462d598b20ceda
fabio parisi <parisifabio98@gmail.com>
2026-02-20 12:34:13 +0100

    pickable plants

--- Interactible/dirt/plantable_dirt.gd
+++ Interactible/dirt/plantable_dirt.gd
@@ -4,13 +4,10 @@ var item_planted: ItemResource
 var dirt_wet: bool = false
 @onready var plant_space: Node3D = $plant_space
 
-var models: PackedScene
+@onready var plant_plant: Area3D = %plant_plant
+@onready var pick_plant: Interactible = %pick_plant
 
-func _on_area_3d_interact() -> void:
-	if item_planted == null:
-		GS.show_plant_norm_seed_interface.emit(true, self)
-	else:
-		print("already planted, take it first")
+var models: PackedScene
 
 func plant_seed(seed: ItemResource):
 	item_planted = seed
@@ -38,6 +35,16 @@ func update_plant():
 			clear_plant_space()
 			plant_space.add_child(model_instance)
 	
+	if item_planted == null:
+		plant_plant.get_child(0).set_deferred("disabled", false)
+		pick_plant.get_child(0).set_deferred("disabled", true)
+	else:
+		if item_planted.type == ItemResource.ItemType.SELLABLE:
+			plant_plant.get_child(0).set_deferred("disabled", true)
+			pick_plant.get_child(0).set_deferred("disabled", false)
+		else:
+			plant_plant.get_child(0).set_deferred("disabled", true)
+			pick_plant.get_child(0).set_deferred("disabled", true)
 	#var model_instance = models.instantiate().duplicate(true)
 	#clear_plant_space()
 	#plant_space.add_child(model_instance)
@@ -45,3 +52,18 @@ func update_plant():
 func clear_plant_space():
 	if plant_space and plant_space.get_child_count() > 0:
 		plant_space.get_child(0).queue_free()
+
+
+func _on_pick_plant_interact() -> void:
+	Inventory.add_item_to_list(item_planted)
+	item_planted = null
+	models = null
+	clear_plant_space()
+	update_plant()
+
+func _on_plant_plant_interact() -> void:
+	if item_planted == null:
+		GS.show_plant_norm_seed_interface.emit(true, self)
+	else:
+		print("already planted, take it first")
+	update_plant()
